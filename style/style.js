9157b4ea8445e896725256a566dfd2c389270320
handikactur <192410101066@students.unej.ac.id>
2020-11-12 00:14:35 +0700

    give some animation

--- style/style.js
+++ style/style.js
@@ -1,3 +1,6 @@
+const modal = document.querySelector('#modal');
+const cardModal = document.querySelector('#modal .card');
+
 window.onscroll = (e) => {
     const navbar = document.querySelector('.navbar');
     if (window.pageYOffset > 194) {
@@ -5,4 +8,19 @@ window.onscroll = (e) => {
     } else {
         navbar.classList.remove('shadow');
     }
+}
+
+const modalToggle = () => {
+    modal.classList.toggle('open');
+    cardModal.classList.toggle('animate');
+}
+
+const submitSeminar = () => {
+    cardModal.children[1].remove();
+    cardModal.innerHTML += `
+    <div class="flex">
+        <span class="fa fa-check-circle fa-5x text-primary"></span>
+        <p style="margin-left: 20px;">Selamat! anda telah terdaftar pada seminar kami. Link room akan kami kirimkan pada email anda dan akan kami ingatkan satu hari sebelum acara. <br> <br> Terima kasih!</p>
+    </div>
+    `
 }
